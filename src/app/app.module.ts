8cefeb9bbf9e45824d2dff64cc28bf35c13c1e2a
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-19 21:36:05 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactusComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
+    CarouselModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  dynamicSlides = [
+    {
+      id: 1,
+      src:'assets/images/school-1.png',
+      alt:'Side 1',
+      title:'Side 1'
+    },
+    {
+      id: 2,
+      src:'assets/images/school-1.png',
+      alt:'Side 2',
+      title:'Side 2'
+    },
+    {
+      id: 3,
+      src:'assets/images/school-1.png',
+      alt:'Side 3',
+      title:'Side 3'
+    },
+    {
+      id: 4,
+      src:'assets/images/school-1.png',
+      alt:'Side 4',
+      title:'Side 4'
+    },
+    {
+      id: 5,
+      src:'assets/images/school-1.png',
+      alt:'Side 5',
+      title:'Side 5'
+    },
+    {
+      id: 6,
+      src:'assets/images/school-1.png',
+      alt:'Side 4',
+      title:'Side 4'
+    },
+    {
+      id: 7,
+      src:'assets/images/school-1.png',
+      alt:'Side 5',
+      title:'Side 5'
+    },
+    {
+      id: 8,
+      src:'assets/images/school-1.png',
+      alt:'Side 4',
+      title:'Side 4'
+    },
+    {
+      id: 9,
+      src:'assets/images/school-1.png',
+      alt:'Side 5',
+      title:'Side 5'
+    },
+    {
+      id: 10,
+      src:'assets/images/school-1.png',
+      alt:'Side 4',
+      title:'Side 4'
+    },
+    {
+      id: 11,
+      src:'assets/images/school-1.png',
+      alt:'Side 5',
+      title:'Side 5'
+    }
+  ]
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 600,
+    navText: ['<i class="las la-angle-left"></i>', '<i class="las la-angle-right"></i>'],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 4
+      },
+      760: {
+        items: 6
+      },
+      1000: {
+        items: 8
+      }
+    },
+    nav: true
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
